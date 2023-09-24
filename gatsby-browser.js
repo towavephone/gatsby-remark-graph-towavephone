b593a4c552762e62d1ff2e0ffc7c59742902a102
tubotao <tubotao@sugo.io>
2023-09-25 00:22:09 +0800

    fix: 修复不支持es6

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,7 @@ exports.onInitialClientRender = function (n, config) {
 
   // load initial script
   const s = document.createElement('script')
-  s.setAttribute('src', `https://unpkg.com/mermaid@${version}/dist/mermaid.min.js`)
+  s.setAttribute('src', 'https://unpkg.com/mermaid@' + version + '/dist/mermaid.min.js')
   document.head.appendChild(s)
 
   // XXX: ugly hack because onRouteUpdate doesn't know when the react is done
