2f42536877239bb7a400275b27a1d6ced5d23e17
tubotao <tubotao@sugo.io>
2023-09-24 23:20:24 +0800

    feat: 增加版本参数

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,10 @@
 exports.onInitialClientRender = function (n, config) {
+  const version = config.version || '8.13.4'
   window.mermaid_config = { theme: config.theme || 'default', startOnLoad: true }
 
   // load initial script
   const s = document.createElement('script')
-  s.setAttribute('src', 'https://unpkg.com/mermaid@8.13.4/dist/mermaid.min.js')
+  s.setAttribute('src', `https://unpkg.com/mermaid@${version}/dist/mermaid.min.js`)
   document.head.appendChild(s)
 
   // XXX: ugly hack because onRouteUpdate doesn't know when the react is done
